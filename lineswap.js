3b855ca9c8a18e96c0fc72cd84b003d46f82918a
Rob Grijseels <robske@gmail.com>
2020-06-29 20:37:25 +0200

    Selection gefixed door return. Range wordt correct bepaald.
    Signed-off-by: Rob Grijseels <robske@gmail.com>

--- lineswap.js
+++ lineswap.js
@@ -1,57 +1,72 @@
 $(document).ready(function() {
-  $('textarea').keydown(function(e) {
-    if (event.ctrlKey) {
-      switch (event.keyCode) {
-        case 38:
-          console.log(getSelectedLine(this, this.selectionStart, 1));
-          break;
-        case 40:
-          console.log(getSelectedLine(this, this.selectionStart, 0));
-          break;
-      }
-    }
-  });
+	$('textarea').keydown(function(e) {
+		if (event.ctrlKey) {
+			switch (event.keyCode) {
+				case 38:
+					return swapLine(this, 1);
+					break;
+				case 40:
+					return swapLine(this, 0);
+					break;
+			}
+		}
+	});
 
-  function getSelectedLine(el, position, direction) {
-    let ret = "",
-      text = el.value;
-    let thisStart = text.substring(0, position).lastIndexOf("\n")
-    let thisEnd = text.substring(position).indexOf("\n") + position;
+	function swapLine(el, direction) {
+		let newRange;
+		let position = el.selectionStart;
+		let oldRangeSize = el.selectionEnd - position;
+		let ret = "", text = el.value;
 
-    let prevStart = text.substring(0, thisStart).lastIndexOf("\n");
-    let nextEnd = text.substring(thisEnd + 1).indexOf("\n") + thisEnd;
+		let thisStart = text.substring(0, position).lastIndexOf("\n")
+		let thisEnd = text.substring(position).indexOf("\n") + position;
 
-    //   if (thisStart === prevStart) {
-    //     console.log("Eerste regel.", prevStart, thisStart, position, thisEnd, nextEnd);
-    //   } else if (thisEnd > nextEnd) {
-    //     console.log("Laatste regel.", prevStart, thisStart, position, thisEnd, nextEnd)
-    //   } else {
-    //     console.log("swap", prevStart, thisStart, position, thisEnd, nextEnd);
-    //   }
-    prevStart = prevStart < 0 ? 0 : prevStart;
-    if (direction) { // Up
-      ret += splitStr(text, 0, prevStart) + "\n|";
-      ret += splitStr(text, thisStart, thisEnd) + "\n|";
-      ret += splitStr(text, prevStart, thisStart) + "\n|";
-      ret += splitStr(text, thisEnd);
-    } else { // Down
-      if (thisStart < 1) ret += splitStr(text, 0, thisStart) + "\n|";
-      ret += splitStr(text, thisEnd, nextEnd) + "\n|";
-      ret += splitStr(text, thisStart, thisEnd) + "\n|";
-      ret += splitStr(text, nextEnd+1);
-    }
-		console.log((direction?"Up":"Down"), prevStart, thisStart, position, thisEnd, nextEnd);
-    console.log(ret);
-  }
+		let prevStart = text.substring(0, thisStart).lastIndexOf("\n");
+		let nextEnd = text.substring(thisEnd + 1).indexOf("\n") + thisEnd + 1;
 
-  function splitStr(text, start, end) {
-    let regex = new RegExp('^\n+|\n+$', 'g'),
-      ret;
-    if (end) {
-      ret = text.substring(start, end);
-    } else {
-      ret = text.substring(start);
-    }
-    return ret.replace(regex, '');
-  }
+		thisEnd = thisEnd < position ? text.length : thisEnd;
+		nextEnd = nextEnd <= thisEnd ? text.length : nextEnd;
+		thisStart = thisStart < 0 ? 0 : thisStart;
+		prevStart = prevStart < 0 ? 0 : prevStart;
+
+		if (direction) { // Up
+			if (thisStart === prevStart) {
+				console.log("Eerste regel.", prevStart, thisStart, position, thisEnd, nextEnd, text.length);
+				return true;
+			} else {
+				if (prevStart > 0) ret += splitStr(text, 0, prevStart) + "\n"; else position--;
+				ret += splitStr(text, thisStart, thisEnd);
+				ret += "\n" + splitStr(text, prevStart, thisStart);
+				if (thisEnd != text.length) ret += "\n" + splitStr(text, thisEnd);
+				newRange = position - thisStart + prevStart;
+			}
+		} else { // Down
+			if (thisEnd === nextEnd) {
+				console.log("Laatste regel.", prevStart, thisStart, position, thisEnd, nextEnd, text.length)
+				return true;
+			} else {
+				if (thisStart > 0) ret += splitStr(text, 0, thisStart) + "\n"; else position++;
+				ret += splitStr(text, thisEnd, nextEnd);
+				ret += "\n" + splitStr(text, thisStart, thisEnd);
+				if (nextEnd != text.length) ret += "\n" + splitStr(text, nextEnd);
+				newRange = position - thisStart + thisEnd;
+			}
+		}
+		el.value = ret;
+		el.focus();
+		el.selectionStart = newRange;
+		el.selectionEnd = newRange + oldRangeSize;
+		return false;
+	}
+
+	function splitStr(text, start, end) {
+		let regex = new RegExp('^\n+|\n+$', 'g'),
+			ret;
+		if (end) {
+			ret = text.substring(start, end);
+		} else {
+			ret = text.substring(start);
+		}
+		return ret.replace(regex, '');
+	}
 });
